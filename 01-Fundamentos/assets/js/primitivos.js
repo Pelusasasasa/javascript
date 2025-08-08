3df960e03deb2710ca32828f6f94922d7ce40a1c
Pelusa-not <agustinlorenzatto@gmail.com>
2025-08-08 19:55:04 -0300

    Comenzado con la parte dos del curso

---
+++ 01-Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,22 @@
+let nombre = 'Peter Parker';
+nombre = 'Ben Parker';
+
+let esMarvel = true;
+
+let edad = 33;
+edad = 33.001;
+
+let superPoder;
+
+let soyNull;
+
+let simbol1 = Symbol('a');
+let simbol2 = Symbol('a');
+
+console.log(typeof nombre);
+console.log(typeof esMarvel);
+console.log(typeof edad);
+console.log(typeof superPoder);
+console.log(typeof soyNull);
+console.log(typeof simbol1);
+console.log(typeof simbol2);
